b83cc1c679e2f3dd4f959821a963a49b1946d21b
Todd <todd@vanillaforums.com>
2014-11-14 11:06:22 -0500

    Format tabs into spaces.

--- Program.cs
+++ Program.cs
@@ -9,191 +9,191 @@ using System.IO;
 using System.Configuration;
 
 namespace dbdump {
-	class Program {
-		static SqlConnection Connection = null;
-		static string Destination = "";
-		static string Destination2 = "";
+    class Program {
+        static SqlConnection Connection = null;
+        static string Destination = "";
+        static string Destination2 = "";
 
-		static void Main(string[] args) {
-			if(args == null || args.Length < 2) {
-				Console.Write("usage: dbdump database destination destination2\n"
-					+ "  database:     The name of the database to export.\n"
-					+ "  destination:  The name of the folder to export all of the files to on the sql server machine.\n"
-					+ "  destination2: The name of the folder that you'll copy the export to on the mysql machine.");
-				return;
-			}
+        static void Main(string[] args) {
+            if (args == null || args.Length < 2) {
+                Console.Write("usage: dbdump database destination destination2\n"
+                    + "  database:     The name of the database to export.\n"
+                    + "  destination:  The name of the folder to export all of the files to on the sql server machine.\n"
+                    + "  destination2: The name of the folder that you'll copy the export to on the mysql machine.");
+                return;
+            }
 
-			string dbname = args[0];
-			Destination = args[1].Replace('\\', '/').TrimEnd('/');
-			if(args.Length > 2)
-				Destination2 = args[2].Replace('\\', '/').TrimEnd('/');
-			else
-				Destination2 = Destination;
+            string dbname = args[0];
+            Destination = args[1].Replace('\\', '/').TrimEnd('/');
+            if (args.Length > 2)
+                Destination2 = args[2].Replace('\\', '/').TrimEnd('/');
+            else
+                Destination2 = Destination;
 
-			SqlConnection connection = null;
+            SqlConnection connection = null;
 
-			try {
-				string cstr = System.Configuration.ConfigurationManager.ConnectionStrings["default"].ConnectionString;
-				//Console.WriteLine(cstr);
-				//return;
-				
-				connection = new SqlConnection(cstr);
-				connection.Open();
-				connection.ChangeDatabase(dbname);
+            try {
+                string cstr = System.Configuration.ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+                //Console.WriteLine(cstr);
+                //return;
 
-				Connection = connection;
-				
+                connection = new SqlConnection(cstr);
+                connection.Open();
+                connection.ChangeDatabase(dbname);
 
-				ExportTableDefs();
-				ExportTables(dbname);
-			} finally {
-				if(connection != null)
-					connection.Close();
-			}
-		}
+                Connection = connection;
 
-		static void ExportTables(string dbName) {
-			string commandFormat = "[{0}].[{1}].[{2}] out "+Destination+"\\{2}.txt -c -C RAW -T -t\"|||COL|||\" -r\"|||ROW|||\" -S{3}";
 
-			String sql = String.Format(@"select * from INFORMATION_SCHEMA.TABLES
+                ExportTableDefs();
+                ExportTables(dbname);
+            } finally {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        static void ExportTables(string dbName) {
+            string commandFormat = "[{0}].[{1}].[{2}] out " + Destination + "\\{2}.txt -c -C RAW -T -t\"|||COL|||\" -r\"|||ROW|||\" -S{3}";
+
+            String sql = String.Format(@"select * from INFORMATION_SCHEMA.TABLES
 				where TABLE_TYPE = 'BASE TABLE'
 					and TABLE_CATALOG = '{0}'", dbName);
-			DataTable table = Execute(sql);
+            DataTable table = Execute(sql);
 
-			foreach(DataRow row in table.Rows) {
-				string tableName = row["TABLE_NAME"].ToString();
-				string schema = row["TABLE_SCHEMA"].ToString();
+            foreach (DataRow row in table.Rows) {
+                string tableName = row["TABLE_NAME"].ToString();
+                string schema = row["TABLE_SCHEMA"].ToString();
 
-				string command = String.Format(commandFormat, dbName, schema, tableName, Connection.DataSource);
+                string command = String.Format(commandFormat, dbName, schema, tableName, Connection.DataSource);
                 Console.WriteLine("bcp " + command);
-				
-				Process.Start("bcp.exe", command);
-			}
-		}
 
-		static void ExportTableDefs() {
-			String sql = "select * from sysobjects where xtype = 'U' order by name";
-			DataTable table = Execute(sql);
+                Process.Start("bcp.exe", command);
+            }
+        }
 
-			if(!Directory.Exists(Destination))
-				Directory.CreateDirectory(Destination);
+        static void ExportTableDefs() {
+            String sql = "select * from sysobjects where xtype = 'U' order by name";
+            DataTable table = Execute(sql);
 
-			FileStream fs = new FileStream(Destination + "/import.sql", FileMode.Create);
-			TextWriter tw = new StreamWriter(fs);
+            if (!Directory.Exists(Destination))
+                Directory.CreateDirectory(Destination);
 
-			foreach(DataRow row in table.Rows) {
-				StringBuilder str = new StringBuilder();
-				string tableName = row["name"].ToString();
+            FileStream fs = new FileStream(Destination + "/import.sql", FileMode.Create);
+            TextWriter tw = new StreamWriter(fs);
 
-				// Grab the columns for the table.
-				string colSql = String.Format(@"select c.*, t.name as typename
+            foreach (DataRow row in table.Rows) {
+                StringBuilder str = new StringBuilder();
+                string tableName = row["name"].ToString();
+
+                // Grab the columns for the table.
+                string colSql = String.Format(@"select c.*, t.name as typename
 					from syscolumns c
 					join systypes t
 						on c.xtype = t.xtype and c.xusertype = t.xusertype
 					where id = {0}
 					order by colid", row["id"]);
 
-				DataTable colTable = Execute(colSql);
-				foreach(DataRow colRow in colTable.Rows) {
-					if(str.Length > 0)
-						str.Append(",\n");
+                DataTable colTable = Execute(colSql);
+                foreach (DataRow colRow in colTable.Rows) {
+                    if (str.Length > 0)
+                        str.Append(",\n");
 
-					str.AppendFormat("`{0}` {1}", colRow["name"], ColumnType(colRow));
-				}
+                    str.AppendFormat("`{0}` {1}", colRow["name"], ColumnType(colRow));
+                }
 
-				// Build the final string.
-				String createSql = String.Format("create table `{0}` (\n{1});\n", tableName, str);
-				tw.WriteLine(createSql);
-				string loadSql = LoadDataSql(tableName);
-				tw.WriteLine(loadSql);
-			}
-			tw.Flush();
-			tw.Close();
-		}
+                // Build the final string.
+                String createSql = String.Format("create table `{0}` (\n{1});\n", tableName, str);
+                tw.WriteLine(createSql);
+                string loadSql = LoadDataSql(tableName);
+                tw.WriteLine(loadSql);
+            }
+            tw.Flush();
+            tw.Close();
+        }
 
-		static string ColumnType(DataRow row) {
+        static string ColumnType(DataRow row) {
             //if (row["name"].ToString() == "Body") {
             //    int foo = 123;
             //}
 
-			string type = row["typename"].ToString().ToLower();
-			if(type == "bigint")
-				return "bigint";
+            string type = row["typename"].ToString().ToLower();
+            if (type == "bigint")
+                return "bigint";
             if (type == "binary")
                 if (Convert.ToInt32(row["length"]) > 255)
                     return "blob";
                 else
-			    	return String.Format("binary({0})", row["length"]);
-			if(type == "bit")
-				return "tinyint";
-			if(type == "char")
-				return String.Format("char({0})", row["length"]);
-			if(type == "datetime" || type == "smalldatetime")
-				return "datetime";
-			if(type == "decimal")
-				return "double";
-			if(type == "float")
-				return "float";
+                    return String.Format("binary({0})", row["length"]);
+            if (type == "bit")
+                return "tinyint";
+            if (type == "char")
+                return String.Format("char({0})", row["length"]);
+            if (type == "datetime" || type == "smalldatetime")
+                return "datetime";
+            if (type == "decimal")
+                return "double";
+            if (type == "float")
+                return "float";
             if (type == "image")
                 return "mediumtext";
-			if(type == "int")
-				return "int";
-			if(type == "nchar")
-				return String.Format("char({0})", row["length"]);
-			//if (type == "nvarchar")
-			//        return String.Format("varchar({0})", row["length"]);
+            if (type == "int")
+                return "int";
+            if (type == "nchar")
+                return String.Format("char({0})", row["length"]);
+            //if (type == "nvarchar")
+            //        return String.Format("varchar({0})", row["length"]);
             if (type == "text" || type == "ntext")
                 return "text";
-			if(type == "real")
-				return "double";
-			if(type == "smallint")
-				return "smallint";
-			if(type == "sql_variant")
-				return "text";
-			if(type == "sysname")
-				return "varchar(255)";
-			if(type == "text")
-				return "text";
-			if(type == "timestamp")
-				return "int";
-			if(type == "tinyint")
-				return "tinyint";
-			if(type == "varbinary") {
-				if(row["length"].ToString() == "-1")
-					return "blob";
-				return String.Format("varbinary({0})", row["length"]);
-			}
-			if(type == "varchar" || type == "nvarchar") {
-				if(row["length"].ToString() == "-1")
-					return "text";
-				return String.Format("varchar({0})", row["length"]);
-			}
+            if (type == "real")
+                return "double";
+            if (type == "smallint")
+                return "smallint";
+            if (type == "sql_variant")
+                return "text";
+            if (type == "sysname")
+                return "varchar(255)";
+            if (type == "text")
+                return "text";
+            if (type == "timestamp")
+                return "int";
+            if (type == "tinyint")
+                return "tinyint";
+            if (type == "varbinary") {
+                if (row["length"].ToString() == "-1")
+                    return "blob";
+                return String.Format("varbinary({0})", row["length"]);
+            }
+            if (type == "varchar" || type == "nvarchar") {
+                if (row["length"].ToString() == "-1")
+                    return "text";
+                return String.Format("varchar({0})", row["length"]);
+            }
 
             Console.WriteLine("Unrecognized type {0}.", type);
 
-			return "varchar(255)";
-		}
+            return "varchar(255)";
+        }
 
-		static DataTable Execute(SqlCommand command) {
-			SqlDataAdapter adapter = new SqlDataAdapter(command);
-			DataTable table = new DataTable();
-			adapter.Fill(table);
-			return table;
-		}
+        static DataTable Execute(SqlCommand command) {
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            return table;
+        }
 
-		static DataTable Execute(string sql) {
-			SqlCommand command = new SqlCommand(sql, Connection);
-			return Execute(command);
-		}
+        static DataTable Execute(string sql) {
+            SqlCommand command = new SqlCommand(sql, Connection);
+            return Execute(command);
+        }
 
-		static string LoadDataSql(string tableName) {
-			string sql = String.Format(@"load data infile '{0}/{1}.txt' into table `{1}`
+        static string LoadDataSql(string tableName) {
+            string sql = String.Format(@"load data infile '{0}/{1}.txt' into table `{1}`
 				character set utf8
 				columns terminated by '|||COL|||'
 				lines terminated by '|||ROW|||'
 				ignore 0 lines;", Destination2, tableName);
 
-			return sql;
-		}
-	}
+            return sql;
+        }
+    }
 }
